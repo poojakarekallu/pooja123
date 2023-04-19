1baeb0ceca4a0b3ceaf3c519dd81b8ecbfca4a50
pooja <poojakarekallu@gmail.com>
2023-04-19 11:38:31 +0530

    text

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+num=int(input("enter a num: "))
+temp=num
+rev=0
+while num!=0:
+    rem=num%10;                                        
+    rev=rev*10+rem;                                       
+    num=num//10; 
+if rev==temp:
+    print("palindrome")
+else:
+    print("not a palindrome")
